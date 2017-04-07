76706ca382d0cfcb7a4199ca8dfe70dd7bf64788
Todd Iverson <tiverson@smumn.edu>
2017-04-07 07:14:48 -0500

    Fixed the deploy script to use subtree push

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,4 @@ touch ./build/stat489/.nojekyll
 git add build/ -f
 git add ./build/stat489/.nojekyll -f
 git commit -m "Added the current build and .nojekyll file as part of deployment"
-git subtree split --prefix build/stat489 -b gh-pages
-git push -f origin gh-pages:gh-pages
-git branch -D gh-pages
+git subtree push --prefix build/stat489 origin gh-pages
